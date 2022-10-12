ad2620546138f4cb9c2b64e65d8761b06facac33
Pedro Henrique Neves da Silva <ptersnow@gmail.com>
2022-10-12 00:29:28 -0400

    Pushing 5 components to storybook

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,38 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
-    extend: {},
+    fontSize: {
+        xs: 14,
+        sm: 16,
+        md: 18,
+        lg: 20,
+        xl: 24,
+        '2xl': 32
+    },
+
+    colors: {
+        'transparent': 'transparent',
+
+        'black': '#000',
+        'white': '#FFF',
+        'gray': {
+            '900': '#121214',
+            '800': '#202024',
+            '400': '#7C7C8A',
+            '200': '#C4C4CC',
+            '100': '#E1E1E6'
+        },
+        'cyan': {
+            '500': '#81D8F7',
+            '300': '#99F1F8'
+        }
+    },
+
+    extend: {
+        fontFamily: {
+            sans: 'Inter, sans-serif'
+        }
+    },
   },
   plugins: [],
 }
